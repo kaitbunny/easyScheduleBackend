cb1adb9ab50962edaa251b6fbcaec87b2d37b99e
Patrik Santos <127355268+kaitbunny@users.noreply.github.com>
2024-04-27 19:55:39 -0300

    feat: implementando validacao para update e insert dos usuarios

--- src/main/java/com/easySchedule/backend/domain/model/Usuario.java
+++ src/main/java/com/easySchedule/backend/domain/model/Usuario.java
@@ -16,6 +16,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -63,4 +65,33 @@ public class Usuario {
     @ManyToOne
     @JoinColumn(name = "turma_id", nullable = true)
     private Turma turma;
+    
+    @PrePersist
+    @PreUpdate
+    public void validateUsuario() {
+    	if(!this.validateProfessor()) {
+    		throw new IllegalArgumentException("Usuarios do tipo Professor nao podem ter turmas");
+    	}
+    	if(!this.validateAluno()) {
+    		throw new IllegalArgumentException("Usuarios do tipo Aluno devem ter uma turma e um curso");
+    	}
+    }
+    
+    public boolean validateProfessor() {
+        if (this.getTipo().equals(TipoUsuario.PROFESSOR)) {
+            if (this.getTurma() != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean validateAluno() {
+        if (this.getTipo().equals(TipoUsuario.ALUNO)) {
+            if (this.getTurma() == null || this.getClass() == null) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
