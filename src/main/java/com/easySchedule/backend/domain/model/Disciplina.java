91c1bff4474f73a1198f624312a58b86ea6f2d27
Patrik Santos <127355268+kaitbunny@users.noreply.github.com>
2024-04-27 20:02:28 -0300

    feat: criacao da casse Disciplina

---
+++ src/main/java/com/easySchedule/backend/domain/model/Disciplina.java
@@ -0,0 +1,25 @@
+package com.easySchedule.backend.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Disciplina {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false)
+    private boolean ativo;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+}
