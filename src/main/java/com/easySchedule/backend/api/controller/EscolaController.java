c495bd85395a975de5a9594aa311a0fca7d104fa
Patrik Santos <127355268+kaitbunny@users.noreply.github.com>
2024-04-28 11:20:45 -0300

    feat: criacao do primeiro endpoint da api

--- src/main/java/com/easySchedule/backend/api/controller/EscolaController.java
+++ src/main/java/com/easySchedule/backend/api/controller/EscolaController.java
@@ -2,9 +2,12 @@ package com.easySchedule.backend.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easySchedule.backend.domain.model.Escola;
 import com.easySchedule.backend.domain.service.CadastroEscolaService;
 
 @CrossOrigin("*")
@@ -14,4 +17,9 @@ public class EscolaController {
 
 	@Autowired
 	private CadastroEscolaService escolaService;
+	
+	@GetMapping("/{id}")
+	public Escola buscar(@PathVariable Long id) {
+		return this.escolaService.buscarOuFalhar(id);
+	}
 }
--- src/main/java/com/easySchedule/backend/domain/service/CadastroEscolaService.java
+++ src/main/java/com/easySchedule/backend/domain/service/CadastroEscolaService.java
@@ -3,6 +3,8 @@ package com.easySchedule.backend.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.easySchedule.backend.domain.exception.notfound.EscolaNaoEncontradaException;
+import com.easySchedule.backend.domain.model.Escola;
 import com.easySchedule.backend.domain.repository.EscolaRepository;
 
 @Service
@@ -10,4 +12,9 @@ public class CadastroEscolaService {
 	
 	@Autowired
 	private EscolaRepository repository;
+	
+	public Escola buscarOuFalhar(Long id) {
+		return repository.findById(id).orElseThrow(() ->
+		new EscolaNaoEncontradaException(id));
+	}
 }
