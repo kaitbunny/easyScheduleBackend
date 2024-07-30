f13a3f049a84bc71d1e2cd08a29c02f916bc82cb
kaitbunny <patrikogit@gmail.com>
2024-07-30 10:03:39 -0300

    feat: curso specification

--- src/main/java/com/easySchedule/backend/domain/specification/CursoSpecification.java
+++ src/main/java/com/easySchedule/backend/domain/specification/CursoSpecification.java
@@ -1,5 +1,42 @@
 package com.easySchedule.backend.domain.specification;
 
-public class CursoSpecification {
+import org.springframework.data.jpa.domain.Specification;
 
+import com.easySchedule.backend.domain.model.Curso;
+import com.easySchedule.backend.domain.model.enums.Periodo;
+
+public class CursoSpecification {
+	
+	public static Specification<Curso> nomeContains(String nome) {
+		return (root, query, criteriaBuilder) -> {
+			if(nome == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+		};
+	}
+	
+	public static Specification<Curso> periodoContains(Periodo periodo) {
+		return (root, query, criteriaBuilder) -> {
+			if(periodo == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.isTrue(criteriaBuilder.function(
+					"JSON_CONTAINS", 
+					Boolean.class,
+					root.get("periodos"),
+					criteriaBuilder.literal("\"" + periodo.name() + "\"")
+			));
+		};
+	}
+	
+	public static Specification<Curso> escolaIdEquals(Long escolaId) {
+		return (root, query, criteriaBuilder) -> {
+			if(escolaId == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.equal(root.get("escola").get("id"), escolaId);
+		};
+	}
+	
 }
