5ddb59946d4a48f5c3e3f3c53e5876f80f465646
kaitbunny <patrikogit@gmail.com>
2024-07-31 10:40:26 -0300

    feat: disciplina controller

--- src/main/java/com/easySchedule/backend/api/controller/DisciplinaController.java
+++ src/main/java/com/easySchedule/backend/api/controller/DisciplinaController.java
@@ -1,11 +1,22 @@
 package com.easySchedule.backend.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.easySchedule.backend.domain.model.Disciplina;
 import com.easySchedule.backend.domain.service.CadastroDisciplinaService;
+import com.easySchedule.backend.utils.paginatedresponse.PaginatedResponse;
 
 @CrossOrigin("*")
 @RestController
@@ -14,4 +25,38 @@ public class DisciplinaController {
 	
 	@Autowired
 	private CadastroDisciplinaService disciplinaService;
+	
+	@GetMapping("/{id}")
+	public Disciplina buscar(@PathVariable Long id) {
+		return this.disciplinaService.buscarOuFalhar(id);
+	}
+	
+	@GetMapping
+	public PaginatedResponse<Disciplina> listar (
+			@RequestParam(name = "page", defaultValue = "1") Integer page,
+			@RequestParam(name = "sortProperty", defaultValue = "id") String sortProperty,
+			@RequestParam(name = "sortDirection", defaultValue = "desc") String sortDirection,
+			@RequestParam(name = "nome", required = false) String nome,
+			@RequestParam(name = "ativo", required = false) Boolean ativo,
+			@RequestParam(name = "cursoId", required = false) Long cursoId) {
+		
+		return this.disciplinaService.listarPorPagina(page, sortProperty, sortDirection, nome, ativo, cursoId);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Disciplina adicionar(@RequestBody Disciplina disciplina) {
+		return this.disciplinaService.salvar(disciplina);
+	}
+	
+	@PutMapping("/{id}")
+	public Disciplina atualizar(@PathVariable Long id, @RequestBody Disciplina disciplina) {
+		return this.disciplinaService.atualizar(id, disciplina);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable Long id) {
+		this.disciplinaService.excluir(id);
+	}
 }
