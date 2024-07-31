60d8b1bed8fabb2d78167f4e1fd0f0a4ba96648b
kaitbunny <patrikogit@gmail.com>
2024-07-31 12:49:00 -0300

    fix: arrumando os imports model sala

--- src/main/java/com/easySchedule/backend/domain/model/Sala.java
+++ src/main/java/com/easySchedule/backend/domain/model/Sala.java
@@ -1,8 +1,15 @@
 package com.easySchedule.backend.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import jakarta.persistence.*;
 
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
