19d1ba08666abfb701df1a6fc1e16a44597f9ae2
kaitbunny <patrikogit@gmail.com>
2024-07-31 10:40:01 -0300

    feat: disciplina specification

--- src/main/java/com/easySchedule/backend/domain/specification/DisciplinaSpecification.java
+++ src/main/java/com/easySchedule/backend/domain/specification/DisciplinaSpecification.java
@@ -1,5 +1,36 @@
 package com.easySchedule.backend.domain.specification;
 
-public class DisciplinaSpecification {
+import org.springframework.data.jpa.domain.Specification;
 
+import com.easySchedule.backend.domain.model.Disciplina;
+
+public class DisciplinaSpecification {
+	
+	public static Specification<Disciplina> nomeContains(String nome) {
+		return (root, query, criteriaBuilder) ->{
+			if(nome == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+		};
+	}
+	
+	public static Specification<Disciplina> isAtivo(Boolean ativo) {
+		return (root, query, criteriaBuilder) -> {
+			if (ativo == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.equal(root.get("ativo"), ativo);
+		};
+	}
+	
+	public static Specification<Disciplina> cursoIdEquals(Long cursoId) {
+		return (root, query, criteriaBuilder) -> {
+			if(cursoId == null) {
+				return criteriaBuilder.conjunction();
+			}
+			return criteriaBuilder.equal(root.get("curso").get("id"), cursoId);
+		};
+	}
+	
 }
