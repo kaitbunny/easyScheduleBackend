b7df03d7f9a450aea0624b1a449bfd6885911eef
Bruno Yoshioka <bruno.yoshioka1995@gmail.com>
2024-11-28 23:28:14 -0300

    feat: sala specification e sala service

--- src/main/java/com/easySchedule/backend/api/controller/SalaController.java
+++ src/main/java/com/easySchedule/backend/api/controller/SalaController.java
@@ -1,9 +1,11 @@
 package com.easySchedule.backend.api.controller;
 
+import com.easySchedule.backend.api.dto.SalaDTO;
+import com.easySchedule.backend.api.mapper.SalaMapper;
+import com.easySchedule.backend.domain.model.Sala;
+import com.easySchedule.backend.utils.paginatedresponse.PaginatedResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.easySchedule.backend.domain.service.CadastroSalaService;
 
@@ -14,4 +16,32 @@ public class SalaController {
 
 	@Autowired
 	private CadastroSalaService salaService;
+
+	@Autowired
+	private SalaMapper salaMapper;
+
+	@GetMapping("/{id}")
+	public SalaDTO buscar(@PathVariable Long id) {
+		var sala = this.salaService.buscarOuFalhar(id);
+		return salaMapper.toDTO(sala);
+	}
+
+	@GetMapping
+	public PaginatedResponse<SalaDTO> listar(
+			@RequestParam(value = "page", defaultValue = "1") Integer page,
+			@RequestParam(value = "sortProperty", defaultValue = "id") String sortProperty,
+			@RequestParam(value = "sortDirection", defaultValue = "desc") String sortDirection,
+			@RequestParam(value = "numero", required = false) String numero,
+			@RequestParam(value = "andar", required = false) String andar,
+			@RequestParam(value = "predio", required = false) String predio,
+			@RequestParam(value = "escolaId", required = false) Long escolaId
+	) {
+		PaginatedResponse<Sala> salas = this.salaService.listarPorPagina(page, sortProperty, sortDirection,
+				numero, andar, predio, escolaId);
+
+		PaginatedResponse<SalaDTO> salaDTOs = salas.map(sala ->
+				salaMapper.toDTO(sala));
+
+		return salaDTOs;
+	}
 }
--- src/main/java/com/easySchedule/backend/domain/specification/SalaSpecification.java
+++ src/main/java/com/easySchedule/backend/domain/specification/SalaSpecification.java
@@ -1,5 +1,42 @@
 package com.easySchedule.backend.domain.specification;
 
-public class SalaSpecification {
+import com.easySchedule.backend.domain.model.Sala;
+import org.springframework.data.jpa.domain.Specification;
 
+public class SalaSpecification {
+    public static Specification<Sala> numeroContains(String numero) {
+        return (root, query, criteriaBuilder) -> {
+            if(numero == null) {
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.like(root.get("numero"), "%" + numero + "%");
+        };
+    }
+
+    public static Specification<Sala> andarContains(String andar) {
+        return (root, query, criteriaBuilder) -> {
+            if(andar == null) {
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.like(root.get("andar"), "%" + andar + "%");
+        };
+    }
+
+    public static Specification<Sala> predioContains(String predio) {
+        return (root, query, criteriaBuilder) -> {
+            if(predio == null) {
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.like(root.get("predio"), "%" + predio + "%");
+        };
+    }
+
+    public static Specification<Sala> escolaIdEquals(Long escolaId) {
+        return (root, query, criteriaBuilder) -> {
+            if(escolaId == null) {
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.equal(root.get("escola").get("id"), escolaId);
+        };
+    }
 }
